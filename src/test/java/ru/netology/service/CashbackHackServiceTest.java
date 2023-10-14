5f0099e4316d66839bde2dc285eb6aa6f97eb461
Julia Nemkina <nemkina.julia@mail.ru>
2023-10-14 21:29:42 +0300

    TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,49 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+
+import static org.testng.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
-    
+    @org.testng.annotations.Test
+    public void calculateRemain () {
+        CashbackHackService service = new CashbackHackService();
+        int  amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void RemainWithCashback () {
+        CashbackHackService service = new CashbackHackService();
+        int  amount = 1800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void RemainWithoutCashback () {
+        CashbackHackService service = new CashbackHackService();
+        int  amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void RemainWithoutActions () {
+        CashbackHackService service = new CashbackHackService();
+        int  amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
 }
